7a60fbfdb7417c6bae6104387be848dc7e8f85ff
fleeB1 <fowler.lee8@gmail.com>
2023-06-25 14:13:56 +0200

    first commit

---
+++ mooty/target/debug/deps/mooty-1ba21e28b1ee3ea0.d
@@ -0,0 +1,8 @@
+/Users/lee/rust/mooty/target/debug/deps/mooty-1ba21e28b1ee3ea0.rmeta: src/lib.rs Cargo.toml
+
+/Users/lee/rust/mooty/target/debug/deps/mooty-1ba21e28b1ee3ea0.d: src/lib.rs Cargo.toml
+
+src/lib.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
---
+++ mooty/target/debug/deps/mooty-92297b6321e55ca2.d
@@ -0,0 +1,8 @@
+/Users/lee/rust/mooty/target/debug/deps/mooty-92297b6321e55ca2.rmeta: src/lib.rs Cargo.toml
+
+/Users/lee/rust/mooty/target/debug/deps/mooty-92297b6321e55ca2.d: src/lib.rs Cargo.toml
+
+src/lib.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
